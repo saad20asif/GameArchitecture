fb933833659857df412fdde75e4f742998f2b639
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-15 16:47:42 +0500

    Default Value option added!

---
+++ Assets/Saad/Variables/Scripts/Float.cs
@@ -0,0 +1,21 @@
+using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "v_", menuName = "ProjectCore/Variables/Float")]
+public class Float : ScriptableObject
+{
+    [SerializeField] protected float Value;
+    [SerializeField] protected float DefaultValue;
+    [SerializeField] protected bool ResetToDefault;
+
+    private void Awake()
+    {
+        if (ResetToDefault)
+            Value = DefaultValue;
+
+    }
+    public void SetValue(float value)
+    { Value = value; }
+    public float GetValue() { return Value; }
+}
