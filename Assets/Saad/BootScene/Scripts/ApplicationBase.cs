ca863dfc6e6b28532d3ae705d4fdb09de4c92690
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-15 16:48:37 +0500

    Scenes modifications!

--- Assets/Saad/BootScene/Scripts/ApplicationBase.cs
+++ Assets/Saad/BootScene/Scripts/ApplicationBase.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplicationBase : MonoBehaviour
 {
     [SerializeField] private int ApplicationFrameRate = 60;
-
     private void Awake()
     {
         Application.targetFrameRate = ApplicationFrameRate;
