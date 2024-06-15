2067beb3caed9fa3913df1ea9709ec685fe09839
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-15 15:10:52 +0500

    fsm in progress!

--- Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
+++ Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
@@ -8,9 +8,6 @@ public class FiniteStateMachine : ScriptableObject, IState
 {
     [SerializeField] private State BootState;
 
-    //[ShowInInspector][NonSerialized] protected State CurrentState;
-    //[ShowInInspector][NonSerialized] protected State PreviousState;
-    [SerializeField] private Stack<State> PausedStates;
     void IState.TransitionTo(State state, Transition transition)
     {
         //Transition(transition);
