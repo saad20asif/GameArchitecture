6d5da10e1daebf0344c052fd6a03af7c04435726
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-16 18:02:16 +0500

    Application flow controller in progress!

---
+++ Assets/Saad/GameFlow/Scripts/ApplicationFlowController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationFlowController : MonoBehaviour
+{
+    [SerializeField] private FiniteStateMachine FiniteStateMachine;
+    private void Start()
+    {
+        ExecuteApplicationFlow();
+    }
+    private void ExecuteApplicationFlow()
+    {
+
+    }
+}
