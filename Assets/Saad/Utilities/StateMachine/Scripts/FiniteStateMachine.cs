f4d16f58d0b82937efa239e6465048e46a168baf
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-16 18:00:42 +0500

    FSM init method for calling bootState done!

--- Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
+++ Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
@@ -1,12 +1,28 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "FiniteStateMachine", menuName = "ProjectCore/State Machine/Basic FSM")]
 public class FiniteStateMachine : ScriptableObject, IState
 {
     [SerializeField] private State BootState;
+    [SerializeField] private State CurrentState;
+
+    
+    public IEnumerator Init()
+    {
+        Debug.Log("FiniteStateMachine Init called");
+        if (BootState != null)
+        {
+            CurrentState = BootState;
+            yield return CurrentState.Init(this);
+            yield return CurrentState.Execute();
+        }
+        else
+        {
+            Debug.LogWarning("Plz pass the boot state in FiniteStateMachine to start the game! ");
+            yield return null;
+        }
+    }
 
     void IState.TransitionTo(State state, Transition transition)
     {
