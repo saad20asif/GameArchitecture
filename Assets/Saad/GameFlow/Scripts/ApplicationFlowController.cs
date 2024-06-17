26e98730eb3997529d909962136db0c3aa01295f
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-17 18:51:36 +0500

    Listening Events in ApplicationFlowController and loading states against that event through FSM!

--- Assets/Saad/GameFlow/Scripts/ApplicationFlowController.cs
+++ Assets/Saad/GameFlow/Scripts/ApplicationFlowController.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class ApplicationFlowController : MonoBehaviour
 {
     [SerializeField] private FiniteStateMachine FiniteStateMachine;
-    private void Start()
-    {
-        ExecuteApplicationFlow();
-    }
-    private void ExecuteApplicationFlow()
-    {
 
+    [Header("MainMenu")]
+    [SerializeField] private GameEvent GoToMainMenuEvent;
+    [SerializeField] private Transition MainMenuTransition;
+    private Coroutine _transitionCo;
+
+    public void Boot()
+    {
+        print("Application Base Called!");
+        GoToMainMenuEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        GoToMainMenuEvent.Handler += GoToMainMenu;
+    }
+    private void OnDisable()
+    {
+        GoToMainMenuEvent.Handler -= GoToMainMenu;
+    }
+    private void GoToMainMenu()
+    {
+        Debug.LogError("GoToMainMenuCalled!");
+        if (_transitionCo != null)
+            StopCoroutine(_transitionCo);
+        _transitionCo = StartCoroutine(FiniteStateMachine.DoTransition(MainMenuTransition));
     }
 }
