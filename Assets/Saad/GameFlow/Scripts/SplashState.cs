ac78dc0dd7e085814a64c84639b43abda749069f
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-17 17:47:36 +0500

    Scene loading logic moved to splash state!

--- Assets/Saad/GameFlow/Scripts/SplashState.cs
+++ Assets/Saad/GameFlow/Scripts/SplashState.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "SplashState", menuName = "ProjectCore/States/SplashState")]
 public class SplashState : State
 {
-    [SerializeField]private Float SceneLoadingProgress;
+    AsyncOperation _asyncLoad;
+    [SerializeField] private Float SceneLoadingProgress;
     private ApplicationFlowController _applicationFlowController;
     public override IEnumerator Enter(IState _listener)
     {
@@ -18,12 +20,22 @@ public class SplashState : State
         yield return base.Execute();
         _applicationFlowController = Instantiate(Resources.Load<ApplicationFlowController>("ApplicationFlowController"));
 
+        yield return GameSceneLoading();
+        // For now im waiting for slider value to reach 1 but later we can also wait for all sdk's to load first(we can map sdk's loading progress from 0-1 and use Int So for that)
+        yield return new WaitUntil(() => SceneLoadingProgress.GetValue() >= 1);
+        _asyncLoad.allowSceneActivation = true;
+
     }
     public override IEnumerator Exit()
     {
         Debug.Log("Splash State Exit has been called!");
         yield return base.Exit();
     }
+    private IEnumerator GameSceneLoading()
+    {
+        _asyncLoad = SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+        _asyncLoad.allowSceneActivation = false;
+        yield break;
+    }
 
-    
 }
