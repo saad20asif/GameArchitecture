08752b4dda89cfc594ddad425e9ee4a9c4ae3059
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-23 22:48:15 +0500

    LevelComplete State script done!

---
+++ Assets/Saad/UI/LevelComplete/Scripts/LevelCompleteState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelCompleteState", menuName = "ProjectCore/State Machine/States/LevelComplete State")]
+public class LevelCompleteState : State
+{
+    [SerializeField] private string prefabName;
+    private GameObject prefabInstance;
+    [SerializeField] private GameEvent GoToLevelFailEvent;
+    public override IEnumerator Enter(IState _listener)
+    {
+        yield return base.Enter(_listener);
+        GameObject prefab = Resources.Load<GameObject>(prefabName);
+        Debug.Log("MainMnu Prefab Loaded!");
+        if (prefab != null)
+        {
+            prefabInstance = Instantiate(prefab);
+        }
+        else
+        {
+            Debug.LogWarning($"Prefab with name {prefabName} could not be found in Resources!");
+        }
+    }
+
+    public override IEnumerator Execute()
+    {
+        yield return base.Execute();
+    }
+
+    public override IEnumerator Exit()
+    {
+        yield return base.Exit();
+        if (prefabInstance != null)
+        {
+            Destroy(prefabInstance);
+        }
+    }
+    public void GoToLevelFail()
+    {
+        GoToLevelFailEvent.Invoke();
+    }
+}
