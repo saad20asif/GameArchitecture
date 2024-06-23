bd41786366593fb3e6d0e39a47d3f0d2703ed94c
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-23 22:54:00 +0500

    Set Active scene as gamescene in splash state!

--- Assets/Saad/GameFlow/Scripts/SplashState.cs
+++ Assets/Saad/GameFlow/Scripts/SplashState.cs
@@ -9,6 +9,7 @@ public class SplashState : State
     AsyncOperation _asyncLoad;
     [SerializeField] private Float SceneLoadingProgress;
     private ApplicationFlowController _applicationFlowController;
+    private string _sceneName="GameScene";
     public override IEnumerator Enter(IState _listener)
     {
         Debug.Log("Splash State init has been called!");
@@ -24,8 +25,25 @@ public class SplashState : State
         // For now im waiting for slider value to reach 1 but later we can also wait for all sdk's to load first(we can map sdk's loading progress from 0-1 and use Int So for that)
         yield return new WaitUntil(() => SceneLoadingProgress.GetValue() >= 1);
         _asyncLoad.allowSceneActivation = true;
+        // Wait for the scene to activate
+        yield return new WaitUntil(() => _asyncLoad.isDone);
+        SetGameSceneAsActiveScene(); // Set the newly loaded game scene as the active scene
+
         _applicationFlowController.Boot();
 
+    }
+    private void SetGameSceneAsActiveScene()
+    {
+        Scene gameScene = SceneManager.GetSceneByName(_sceneName);
+        if (gameScene.IsValid())
+        {
+            SceneManager.SetActiveScene(gameScene);
+            Debug.Log("Game scene activated successfully.");
+        }
+        else
+        {
+            Debug.LogWarning("Game scene is not valid.");
+        }
     }
     public override IEnumerator Exit()
     {
@@ -34,7 +52,7 @@ public class SplashState : State
     }
     private IEnumerator GameSceneLoading()
     {
-        _asyncLoad = SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+        _asyncLoad = SceneManager.LoadSceneAsync(_sceneName, LoadSceneMode.Additive);
         _asyncLoad.allowSceneActivation = false;
         yield break;
     }
