549c7b346f1fbc6309ce65417d28c3f5d21444e0
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-23 22:48:31 +0500

    LevelComplete Transition done!

---
+++ Assets/Saad/UI/LevelComplete/Scripts/LevelCompleteTransition.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelCompleteTransition", menuName = "ProjectCore/State Machine/Transitions/LevelComplete Transition")]
+public class LevelCompleteTransition : Transition
+{
+    
+}
