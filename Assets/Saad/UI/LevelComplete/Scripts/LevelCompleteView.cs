873dc111876d846a83c0dd2e713c37c49715630e
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-06-23 22:48:47 +0500

    LevelComplete View done!

---
+++ Assets/Saad/UI/LevelComplete/Scripts/LevelCompleteView.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompleteView : MonoBehaviour
+{
+    [SerializeField] private Button NextBtn;
+    [SerializeField] private LevelCompleteState LevelCompleteState;
+
+    private void OnEnable()
+    {
+        NextBtn.onClick.AddListener(OnNextBtnClicked);
+    }
+    private void OnDisable()
+    {
+        NextBtn.onClick.RemoveListener(OnNextBtnClicked);
+    }
+    private void OnNextBtnClicked()
+    {
+        LevelCompleteState.GoToLevelFail();
+    }
+}
