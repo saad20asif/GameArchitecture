0c0353734c2e15bb289bf2c053bc46f1e3805236
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-03 23:27:38 +0500

    Bool So!

---
+++ Assets/Saad/Variables/Scripts/Bool.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "vBool_", menuName = "ProjectCore/Variables/Bool")]
+public class Bool : ScriptableObject
+{
+    [SerializeField] protected bool Value;
+    [SerializeField] protected bool DefaultValue;
+    [SerializeField] protected bool ResetToDefaultOnPlay;
+
+    private void OnEnable()
+    {
+        if (ResetToDefaultOnPlay)
+            Value = DefaultValue;
+
+    }
+    public virtual void SetValue(bool value)
+    { Value = value; }
+    public virtual bool GetValue() { return Value; }
+}
