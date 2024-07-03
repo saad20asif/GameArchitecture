d6f273545068e560eed657dd65ab261d17d2ecf7
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-03 23:09:35 +0500

    Double So!

---
+++ Assets/Saad/Variables/Scripts/Double.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "vDouble_", menuName = "ProjectCore/Variables/Double")]
+public class Double : ScriptableObject
+{
+    [SerializeField] protected double Value;
+    [SerializeField] protected double DefaultValue;
+    [SerializeField] protected bool ResetToDefaultOnPlay;
+
+    private void OnEnable()
+    {
+        if (ResetToDefaultOnPlay)
+            Value = DefaultValue;
+
+    }
+    public virtual void SetValue(double value)
+    { Value = value; }
+    public virtual double GetValue() { return Value; }
+}
