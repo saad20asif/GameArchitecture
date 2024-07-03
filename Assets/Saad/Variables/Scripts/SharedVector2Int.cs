f9768b1bc13b395ef957505dd5063725e6ea2a5e
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-03 23:46:40 +0500

    SharedVector2Int So!

---
+++ Assets/Saad/Variables/Scripts/SharedVector2Int.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "vSharedVector2Int_", menuName = "ProjectCore/Variables/Simple/SharedVector2Int")]
+public class SharedVector2Int : ScriptableObject
+{
+    [SerializeField] protected Vector2Int Value;
+    [SerializeField] protected Vector2Int DefaultValue;
+    [SerializeField] protected bool ResetToDefaultOnPlay;
+
+    private void OnEnable()
+    {
+        if (ResetToDefaultOnPlay)
+            Value = DefaultValue;
+
+    }
+    public virtual void SetValue(Vector2Int value)
+    { Value = value; }
+    public virtual Vector2Int GetValue() { return Value; }
+}
