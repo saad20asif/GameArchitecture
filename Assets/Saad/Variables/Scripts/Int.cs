861736fc4d4721521a3afec2afcd93e90412d050
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-03 23:08:59 +0500

    Int So! Increment Decrement functions also added!

---
+++ Assets/Saad/Variables/Scripts/Int.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "vInt_", menuName = "ProjectCore/Variables/Int")]
+public class Int : ScriptableObject
+{
+    [SerializeField] protected int Value;
+    [SerializeField] protected int DefaultValue;
+    [SerializeField] protected bool ResetToDefaultOnPlay;
+
+    private void OnEnable()
+    {
+        if (ResetToDefaultOnPlay)
+            Value = DefaultValue;
+
+    }
+    public virtual void SetValue(int value)
+    { Value = value; }
+
+    public virtual void Increment(int _increment)
+    { Value += _increment; }
+
+    public virtual void Decrement(int _decrement)
+    { Value -= _decrement; }
+    public virtual float GetValue() { return Value; }
+}
