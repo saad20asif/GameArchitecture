1ebdc278b6bf5859e43cd57ef6fedbac715648cc
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-07 21:35:24 +0500

    Resume Pause handled for now!

--- Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
+++ Assets/Saad/Utilities/StateMachine/Scripts/FiniteStateMachine.cs
@@ -1,6 +1,5 @@
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,57 +8,93 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FiniteStateMachine", menuName = "ProjectCore/State Machine/Basic FSM")]
 public class FiniteStateMachine : SerializedScriptableObject, IState
 {
-    [SerializeField] private State BootState;
-    [SerializeField] private State CurrentState;
-    [SerializeField] private Stack<State> PausedStates;
+    [SerializeField] private State BootState; // The initial state of the FSM
+    [SerializeField] private State CurrentState; // The current active state
+    [SerializeField] private Stack<State> PausedStates = new Stack<State>(); // Stack to manage paused states
 
     // Initializes the FiniteStateMachine with the BootState
     public IEnumerator Init()
     {
         Debug.Log("FiniteStateMachine Init called");
-        if (BootState != null)
-        {
-            CurrentState = BootState;
-            // Pass 'this' (the FiniteStateMachine instance) to the BootState
-            yield return CurrentState.Enter(this);
 
-            if(PausedStates != null) { PausedStates.Clear(); }
-            else
-                PausedStates = new Stack<State>();
-        }
-        else
+        if (BootState == null)
         {
             Debug.LogWarning("Please assign a boot state in FiniteStateMachine to start the game!");
-            yield return null;
+            yield break;
         }
+
+        CurrentState = BootState;
+        yield return CurrentState.Enter(this);
+        PausedStates.Clear(); // Clear paused states stack
     }
 
     // Implements the TransitionTo method from the IState interface
     public void TransitionTo(Transition transition)
     {
-        if (transition != null && transition.ToState != null)
-            CoroutineRunner.instance.StartCoroutine(DoTransition(transition));
+        if (transition == null || transition.ToState == null)
+        {
+            Debug.LogWarning("Invalid transition or target state.");
+            return;
+        }
+
+        CoroutineRunner.instance.StartCoroutine(DoTransition(transition));
     }
 
     // Performs the state transition
     private IEnumerator DoTransition(Transition transition)
     {
         Debug.Log("DoTransition " + transition.ToState.name);
-        State _nextState = transition.ToState;
+
         if (CurrentState != null)
         {
-            if(_nextState.PausePreviousState)
+            State nextState = transition.ToState;
+
+            if (nextState.PausePreviousState)
             {
+                // Pause the current state and push it onto the stack
+                Debug.Log("Pausing current state.");
                 yield return CurrentState.Pause();
                 PausedStates.Push(CurrentState);
             }
             else
-                yield return CurrentState.Exit();
+            {
+                // If the next state is not found in the paused stack, clear the stack
+                if (!IsStateInPausedStack(nextState))
+                {
+                    Debug.Log("Clearing all paused states.");
+                    while (PausedStates.Count > 0)
+                    {
+                        State pausedState = PausedStates.Pop();
+                        yield return pausedState.Exit();
+                    }
+                }
 
-            CurrentState = _nextState;
-            // Pass 'this' (the FiniteStateMachine instance) to the new state,  so that any state can call Transition directly too!
-            yield return CurrentState.Enter(this);
+                // Exit the current state
+                yield return CurrentState.Exit();
+            }
+
+            CurrentState = nextState;
+
+            if (IsStateInPausedStack(nextState))
+            {
+                // Resume the next state if it was previously paused
+                Debug.Log("Resuming paused state: " + nextState.name);
+                yield return nextState.Resume();
+                PausedStates.Pop();
+            }
+            else
+            {
+                // Enter the new state if it was not paused
+                yield return CurrentState.Enter(this);
+            }
         }
-        yield return null;
+    }
+
+    // Checks if the specified state is in the paused states stack
+    private bool IsStateInPausedStack(State nextState)
+    {
+        //if(PausedStates.Count > 0)
+        //Debug.Log("peek : " + PausedStates.Peek()+ "  nextState : "+ nextState);
+        return PausedStates.Count > 0 && PausedStates.Peek() == nextState;
     }
 }
