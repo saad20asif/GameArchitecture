ea50f57f636d6e95e3db1e1957507016ef44b03c
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-07 19:02:02 +0500

    MainMenu has spinwheel btn to load spin wheelstate!

--- Assets/Saad/UI/MainMenu/Scripts/MainMenuView.cs
+++ Assets/Saad/UI/MainMenu/Scripts/MainMenuView.cs
@@ -5,18 +5,24 @@ public class MainMenuView : MonoBehaviour
 {
     [SerializeField] private MainMenuState MainMenuState;
     [SerializeField] private Button PlayBtn;
-    //[SerializeField] private LevelCompleteS
+    [SerializeField] private Button GoToSpinWheel;
 
     private void OnEnable()
     {
         PlayBtn.onClick.AddListener(OnNextBtnClicked);
+        GoToSpinWheel.onClick.AddListener(OnSpinWheelClicked);
     }
     private void OnDisable()
     {
         PlayBtn.onClick.RemoveListener(OnNextBtnClicked);
+        GoToSpinWheel.onClick.RemoveListener(OnSpinWheelClicked);
     }
     private void OnNextBtnClicked()
     {
         MainMenuState.GoToPlayState();
+    }
+    private void OnSpinWheelClicked()
+    {
+        MainMenuState.GoToSpinWheel();
     }
 }
