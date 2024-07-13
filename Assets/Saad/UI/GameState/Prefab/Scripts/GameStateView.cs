9cc6179f0d5da78273ff167890f1c3bb6010d11a
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-14 00:56:59 +0500

    Time only shows in game state when we are in game state and its not paused!

--- Assets/Saad/UI/GameState/Prefab/Scripts/GameStateView.cs
+++ Assets/Saad/UI/GameState/Prefab/Scripts/GameStateView.cs
@@ -1,4 +1,7 @@
+using ProjectCore.Events;
+using ProjectCore.TimeUtility;
 using ProjectCore.UI;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,16 +10,22 @@ public class GameStateView : UiBase
     [SerializeField] private Button LevelCompleteBtn;
     [SerializeField] private Button LevelFailBtn;
     [SerializeField] private GameState GameState;
+    [SerializeField] private GameEvent Tick;
+    [SerializeField] private TextMeshProUGUI TimeText;
+    int seconds = 0;
 
     private void OnEnable()
     {
+        UpdateTime();
         LevelCompleteBtn.onClick.AddListener(OnLevelCompleteBtn);
         LevelFailBtn.onClick.AddListener(OnLevelFailBtn);
+        Tick.Handler += UpdateTime;
     }
     private void OnDisable()
     {
         LevelCompleteBtn.onClick.RemoveListener(OnLevelCompleteBtn);
         LevelFailBtn.onClick.RemoveListener(OnLevelFailBtn);
+        Tick.Handler -= UpdateTime;
     }
     private void OnLevelCompleteBtn()
     {
@@ -25,5 +34,13 @@ public class GameStateView : UiBase
     private void OnLevelFailBtn()
     {
         GameState.GoToLevelFailState();
+    }
+    private void UpdateTime()
+    {
+        if(!Paused)
+        {
+            seconds++;
+            TimeText.text = TimeManager.FormatTime(seconds);
+        }
     }
 }
