d662ce5991c3fc222ba55d14afea27b045291586
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-17 23:46:04 +0500

    KeyRegistry class for generating unique keys for playerPrefs auto keys generations!

---
+++ Assets/Saad/Variables/Helper/KeyRegistry.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace ProjectCore.Utilities
+{
+    public static class KeyRegistry
+    {
+        private static HashSet<string> keys = new HashSet<string>();
+        private static int counter = 0;
+
+        public static string GenerateKey(string prefix = "DBInt")
+        {
+            string uniqueKey;
+            do
+            {
+                uniqueKey = $"{prefix}_{counter}";
+                counter++;
+            } while (keys.Contains(uniqueKey));
+
+            keys.Add(uniqueKey);
+            return uniqueKey;
+        }
+
+        public static void UnregisterKey(string key)
+        {
+            keys.Remove(key);
+        }
+    }
+}
