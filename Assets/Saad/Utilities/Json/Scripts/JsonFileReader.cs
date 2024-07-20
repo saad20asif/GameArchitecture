6b280006af79fd4d9a0f294cccedd91e6bdd7643
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-21 00:45:58 +0500

    JsonFileReader implementing IJsonReader interface!

---
+++ Assets/Saad/Utilities/Json/Scripts/JsonFileReader.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+public class JsonFileReader : IJsonReader
+{
+    public T Read<T>(string filepath)
+    {
+        if(!File.Exists(filepath))
+        {
+            throw new FileNotFoundException("File Not Found! ",filepath);
+        }
+        string _jsonContent = File.ReadAllText(filepath);
+        return JsonConvert.DeserializeObject<T>(_jsonContent);
+    }
+}
