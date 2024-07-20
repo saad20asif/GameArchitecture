e21aa93f9e0d23b34b88f86af70a7cfa263b442c
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-07-21 00:54:21 +0500

    Students Json Handler class!

--- Assets/Saad/Utilities/Json/Scripts/JsonFileReader.cs
+++ Assets/Saad/Utilities/Json/Scripts/JsonFileReader.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.IO;
 using Newtonsoft.Json;
 public class JsonFileReader : IJsonReader
---
+++ Assets/StudentsJsonHandler.cs
@@ -0,0 +1,90 @@
+using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Bson;
+
+[Serializable]
+public struct Address
+{
+    public string street;
+    public string city;
+    public string state;
+    public string postalCode;
+}
+
+[Serializable]
+public struct Course
+{
+    public string courseId;
+    public string courseName;
+    public string instructor;
+}
+
+[Serializable]
+public struct Grades
+{
+    public int math;
+    public int science;
+    public int english;
+    public int history;
+}
+
+[Serializable]
+public struct Student
+{
+    public int id;
+    public string name;
+    public int age;
+    public string email;
+    public Address address;
+    public Grades grades;
+    public Course[] courses;
+}
+
+[Serializable]
+public struct StudentWrapper
+{
+    public Student[] students;
+}
+
+public class StudentsJsonHandler : MonoBehaviour
+{
+    [SerializeField] private string FilePath;
+    [SerializeField] private Student[] Students;
+
+    private IJsonReader _jsonReader;
+
+    // Dependency injection via constructor or method injection can be used.
+    // Here we use setter injection for simplicity.
+    //public void SetJsonReader(IJsonReader jsonReader)
+    //{
+    //    _jsonReader = jsonReader;
+    //}
+    //private void Awake()
+    //{
+    //    _jsonReader = new JsonFileReader();
+    //}
+    [Button]
+    private void LoadJson()
+    {
+        _jsonReader = new JsonFileReader();
+
+        if (_jsonReader == null)
+        {
+            Debug.LogError("IJsonReader not set!");
+            return;
+        }
+
+        try
+        {
+            StudentWrapper studentWrapper = _jsonReader.Read<StudentWrapper>(FilePath);
+            Students = studentWrapper.students;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Failed to load JSON: {ex.Message}");
+        }
+    }
+}
