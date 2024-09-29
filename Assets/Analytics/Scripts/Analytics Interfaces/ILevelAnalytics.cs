7d428265cf2f38ce974961e0efbf41fb94cd3f31
Talha Naveed <talha.naveed.hazel.mobile@gmail.com>
2024-09-29 14:25:07 +0500

    Level Analytics Changes

--- Assets/Analytics/Scripts/Analytics Interfaces/ILevelAnalytics.cs
+++ Assets/Analytics/Scripts/Analytics Interfaces/ILevelAnalytics.cs
@@ -1,4 +1,13 @@
 public interface ILevelAnalytics 
 {
-   
+    public void LevelStart(int levelNo, string levelMode);
+
+    public void LevelPaused(int levelNo, string levelMode, string levelTime);
+
+    public void LevelResumed(int levelNo, string levelMode, string levelTime);
+
+    public void LevelFailed(int levelNo, string levelMode, string levelTime);
+    public void LevelComplete(int levelNo, string levelMode, string levelTime);
+
+    public void LevelRestart(int levelNo, string levelMode, string levelTime);
 }
