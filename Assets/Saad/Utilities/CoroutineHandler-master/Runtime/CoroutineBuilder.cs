9d7897a8c978005b6d55db7d8849f011a824bc11
Saad Asif <saadasif@gamedistrict.co>
2025-05-05 14:55:39 +0500

    Coroutine Handler utility

---
+++ Assets/Saad/Utilities/CoroutineHandler-master/Runtime/CoroutineBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace THEBADDEST.Coroutines
+{
+
+
+	public class CoroutineBuilder
+	{
+
+		readonly List<CoroutineSequence> sequences = new();
+
+		public void Run()
+		{
+			CoroutineHandler.AfterWait(sequences.ToArray());
+		}
+
+		public CoroutineBuilder AfterWait(CoroutineMethod action, float seconds, bool realTime = false)
+		{
+			sequences.Add(new CoroutineDelay(action, seconds, realTime));
+			return this;
+		}
+
+		public CoroutineBuilder AfterWait(CoroutineMethod action, Func<bool> condition)
+		{
+			sequences.Add(new CoroutineCondition(action, condition));
+			return this;
+		}
+
+		public CoroutineBuilder WaitLoop(CoroutineMethod action, Func<bool> condition, float seconds = 0, bool realTime = false)
+		{
+			sequences.Add(new CoroutineLoop(action, condition, seconds, realTime));
+			return this;
+		}
+
+	}
+
+
+}
