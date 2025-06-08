e887136531e1f6b715c0599b16c6b12841464437
SaadAsif <saad20asif@gmail.com>
2025-06-08 04:27:42 -0700

    Timed Transition added

---
+++ Assets/Saad/Utilities/StateMachine/Scripts/TimedTransition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+namespace ProjectCore.StateMachine
+{
+    [CreateAssetMenu(fileName = "TimedTransition", menuName = "ProjectCore/State Machine/Transitions/Timed")]
+    public class TimedTransition : Transition
+    {
+        [SerializeField] private float delayInSeconds = 1f;
+
+        public override IEnumerator Execute()
+        {
+            yield return new WaitForSeconds(delayInSeconds);
+        }
+    }
+}
